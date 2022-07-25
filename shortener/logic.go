4cfbf882aa81b000efe7691d470d48b05ad49414
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2022-07-25 21:52:36 +0100

    initialize app and domain logic

---
+++ shortener/logic.go
@@ -0,0 +1,37 @@
+package shortener
+
+import (
+	"errors"
+	errs "github.com/pkg/errors"
+	"github.com/teris-io/shortid"
+	"gopkg.in/dealancer/validate.v2"
+	"time"
+)
+
+var (
+	ErrRedirectNotFound = errors.New("Redirect Not Found")
+	ErrRedirectInvalid = errors.New("Redirect Invalid")
+)
+
+type redirectService struct {
+	redirectRepo RedirectRepository
+}
+
+func NewRedirectService(repo RedirectRepository) RedirectService {
+	return &redirectService{
+		redirectRepo: repo,
+	}
+}
+
+func (r *redirectService) Find(code string) (*Redirect, error) {
+	return r.redirectRepo.Find(code)
+}
+
+func (r *redirectService) Store(redirect *Redirect) error {
+	if err := validate.Validate(redirect); err != nil {
+		errs.Wrap(ErrRedirectInvalid, "service.Redirect.Store")
+	}
+	redirect.Code = shortid.MustGenerate()
+	redirect.CreatedAt = time.Now().UTC().Unix()
+	return r.redirectRepo.Store(redirect)
+}
